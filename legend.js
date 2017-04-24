a7102b78433aa8a7e718b173e3606f2414c22555
Jack Williams <jw2249a@users.noreply.github.com>
2017-04-24 09:37:08 -0400

    Update legend.js

--- legend.js
+++ legend.js
@@ -2,7 +2,7 @@
 var legend = L.control({position: 'bottomright'});
 legend.onAdd = function(map) {
 	var div = L.DomUtil.create('div', 'legend');
-	var labels1 = ["NA", "80 to 100", "50 to 80", "20 to 50", "10 to 20", "5 to 10", "2 to 5", "1 to 2", "Less than 1", "0"];
+	var labels1 = ["NA", "More than 80", "50 to 80", "20 to 50", "10 to 20", "5 to 10", "2 to 5", "1 to 2", "Less than 1", "0"];
 			var grades1 = ["MA", "i", "h", "g", "f", "e", "d", "c", "b", "a"];
 			 div.innerHTML +='Rest/Sec Rate (per 100)<br>'
 			for (var i = grades1.length-2; i >= 0; i--) {
